31fa739f57e9ca3cf253b2ff0d144c0dff908321
Devilblack066 <44459633+Devilblack066@users.noreply.github.com>
2019-10-22 14:24:41 +0200

    test 2

--- Assets/Scripts/Canon3DScript.cs
+++ Assets/Scripts/Canon3DScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Canon3DScript : MonoBehaviour
 {
+    public GameObject PivotCanon;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,21 @@ public class Canon3DScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Ray MyRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        /*if (Physics.Raycast(MyRay, out hit))
+        {
+            hit.collider.renderer.material.color = Color.red;
+            //Debug.Log(hit);
+        }*/
+        RaycastHit hit;
+        if (Physics.Raycast(MyRay, out hit))
+        {
+            /*hit.collider.GetComponent<Renderer>().material.color = Color.red;*/
+            Debug.Log(hit.point);
+            PivotCanon.transform.LookAt(hit.point);
+            PivotCanon.transform.eulerAngles = new Vector3(0, PivotCanon.transform.eulerAngles.y, 0);
+        }
+        //transform.position = MyRay.
     }
 }
