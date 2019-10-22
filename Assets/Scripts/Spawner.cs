dcf669f0a1ef95f758215008825d5ed8e3fbf5c2
loic disarbois <loic.disarbois@gmail.com>
2019-10-22 15:51:47 +0200

    15:51

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+
+    public Transform[] target;
+
+
+
+    [SerializeField] public GameObject PrefabEnemy;
+
+    [SerializeField] public int InitialSpoolCount;
+
+    [SerializeField] public int spawnTime;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i=0;i<InitialSpoolCount;i++){
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+        void Spawn ()
+    {
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        GameObject EnnemySPWND = Instantiate (PrefabEnemy,this.transform.position ,this.transform.rotation);
+        EnnemySPWND.GetComponent<Patrol>().setPatrols(target);
+   
     }
 }
