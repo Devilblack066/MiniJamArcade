3192a8488df1011f9f5d555625995764b454bb28
Devilblack066 <44459633+Devilblack066@users.noreply.github.com>
2019-10-23 15:16:02 +0200

    HUD

---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HUDScript : MonoBehaviour
+{
+    public GameObject Canon;
+    private Canon3DScript CanonCD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CanonCD = Canon.GetComponent<Canon3DScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CanonCD = Canon.GetComponent<Canon3DScript>().T;
+        GetComponentInChildren<Image>().fillAmount = CanonCD.TimerShot / CanonCD.CanonDelayShot;
+    }
+}
